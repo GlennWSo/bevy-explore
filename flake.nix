56986fc8ea4f1e67f4568f6fcc3affcf0dce664c
GlennWSo <sondellglenn@gmail.com>
2024-06-11 21:16:45 +0200

    nix wrap and init web

--- flake.nix
+++ flake.nix
@@ -3,8 +3,16 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    rust-overlay.url = "github:oxalica/rust-overlay";
     flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+    };
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = {
@@ -12,6 +20,7 @@
     nixpkgs,
     rust-overlay,
     flake-utils,
+    crane,
   }:
     flake-utils.lib.eachDefaultSystem (
       system: let
@@ -24,7 +33,37 @@
         rust = pkgs.rust-bin.nightly.latest.default.override {
           targets = ["wasm32-unknown-unknown"];
         };
+        craneLib = (crane.mkLib pkgs).overrideToolchain rust;
+        fs = pkgs.lib.fileset;
+        files = fs.unions [
+          ./.cargo
+          ./src
+          ./assets
+          (fs.fileFilter (file: file.hasExt "toml") ./.)
+          (fs.fileFilter (file: file.name == "Cargo.lock") ./.)
+        ];
 
+        src = fs.toSource {
+          root = ./.;
+          fileset = files;
+        };
+
+        cargoArtifacts = craneLib.buildDepsOnly {
+          inherit src buildInputs;
+          version = "0.1.0";
+          doCheck = false;
+
+          # cargoExtraArgs = "--target thumbv7em-none-eabihf";
+        };
+
+        nixCrate = craneLib.buildPackage {
+          inherit src cargoArtifacts buildInputs;
+          postFixup = ''
+            cp -r assets $out/bin/
+            wrapProgram $out/bin/${nixCrate.pname} \
+              --set LD_LIBRARY_PATH ${LIBS}
+          '';
+        };
         libs = with pkgs; [
           alsaLib
           udev
@@ -32,7 +71,7 @@
           wayland
           libxkbcommon
         ];
-        buildDeps = with pkgs;
+        buildInputs = with pkgs;
           [
             pkg-config
             rust
@@ -43,6 +82,7 @@
             vulkan-loader
             vulkan-validation-layers
             wasm-bindgen-cli
+            makeWrapper
           ]
           ++ libs;
 
@@ -61,11 +101,13 @@
         '';
       in
         with pkgs; {
+          packages.default = nixCrate;
+          packages.deps = cargoArtifacts;
           devShells.default = mkShell {
             inherit LIBS;
             name = "rust graphics env";
             WINIT_UNIX_BACKEND = "wayland";
-            buildInputs = buildDeps ++ utils ++ [run];
+            buildInputs = buildInputs ++ utils ++ [run];
             shellHook = ''
               echo Entering rust env!
             '';
