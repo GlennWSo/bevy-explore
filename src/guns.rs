aff348693301d145dcd25a96c7a99100ece40d64
GlennWSo <sondellglenn@gmail.com>
2024-07-29 14:42:44 +0200

    init evert rewrite

--- src/guns.rs
+++ src/guns.rs
@@ -1,7 +1,11 @@
+use std::marker::PhantomData;
+
 use avian2d::prelude::*;
+use bevy::reflect::Tuple;
 use bevy::{audio::Volume, prelude::*, sprite::MaterialMesh2dBundle};
 
 use crate::collide_dmg::CollisionDamage;
+use crate::ship::SpaceShip;
 use crate::{
     assets::MyAssets,
     despawn::despawn_far,
@@ -20,8 +24,10 @@ impl Plugin for GunPlugin {
             Update,
             cooldown_guns::<PlasmaGun>.in_set(InGameSet::EntityUpdate),
         )
+        .add_systems(Update, handle_plasma_gun_fire)
         .add_systems(Update, ship_weapon_ctrl.in_set(InGameSet::UI))
         .add_systems(Update, despawn_far::<Plasma, 10_000>);
+        app.add_event::<GunFire>();
     }
 }
 
@@ -124,104 +130,126 @@ impl Gun for PlasmaGun {
     }
 }
 
-fn ship_weapon_ctrl(
-    mut cmds: Commands,
-    mut q: Query<(&Transform, &LinearVelocity, &mut PlasmaGun), With<Player>>,
-    btn_input: Res<ButtonInput<KeyCode>>,
-    // time: Res<Time>,
-    assets: Res<MyAssets>,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<ColorMaterial>>,
-) {
-    if btn_input.pressed(KeyCode::Space) {
-        let Ok((ship_transform, ship_velocity, mut plasma_gun)) = q.get_single_mut() else {
-            return;
-        };
-        fire_plasma(
-            plasma_gun,
-            ship_transform,
-            ship_velocity,
-            &mut materials,
-            &mut meshes,
-            &assets,
-            &mut cmds,
-        );
-    }
-    if btn_input.pressed(KeyCode::ControlLeft) {
-        let Ok((ship_transform, ship_velocity, mut plasma_gun)) = q.get_single_mut() else {
-            return;
-        };
-        println!("Fire hook");
-        // fire_plasma(
-        //     plasma_gun,
-        //     ship_transform,
-        //     ship_velocity,
-        //     &mut materials,
-        //     &mut meshes,
-        //     &assets,
-        //     &mut cmds,
-        // );
+#[derive(Event)]
+struct GunFire {
+    phantom: PhantomData<PlasmaGun>,
+    entity: Entity,
+    origin: Vec2,
+}
+
+impl GunFire {
+    fn new(entity: Entity, origin: Vec2) -> Self {
+        Self {
+            entity,
+            origin,
+            phantom: PhantomData,
+        }
     }
 }
 
-fn fire_plasma(
-    mut plasma_gun: Mut<PlasmaGun>,
-    ship_transform: &Transform,
-    ship_velocity: &LinearVelocity,
-    materials: &mut ResMut<Assets<ColorMaterial>>,
-    meshes: &mut ResMut<Assets<Mesh>>,
-    assets: &Res<MyAssets>,
-    cmds: &mut Commands,
+fn ship_weapon_ctrl(
+    // mut cmds: Commands,
+    q: Query<(Entity, &Transform), With<Player>>,
+    mut writer: EventWriter<GunFire>,
+
+    btn_input: Res<ButtonInput<KeyCode>>,
+    // time: Res<Time>,
+    // assets: Res<MyAssets>,
+    // mut meshes: ResMut<Assets<Mesh>>,
+    // mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
-    let Some(plasma) = plasma_gun.fire() else {
+    let Ok((entity, ship_transform)) = q.get_single() else {
         return;
     };
+    let transform = ship_transform.translation - ship_transform.up() * FORWARD_OFFSET;
+    let origin = transform.truncate();
+    if btn_input.pressed(KeyCode::Space) {
+        writer.send(GunFire::new(entity, origin));
+    }
+    if btn_input.pressed(KeyCode::ControlLeft) {
+        let Ok((entity, _)) = q.get_single() else {
+            return;
+        };
+        println!("Fire hook from: {}", entity);
+    }
+}
 
-    let mut transform = ship_transform.with_scale(Vec3::ONE);
-    let velocity: LinearVelocity =
-        (-transform.up().truncate() * Plasma::SPEED + **ship_velocity).into();
-    transform.translation -= FORWARD_OFFSET * *ship_transform.up();
+trait MkMissle {
+    type MissleBundle: Bundle;
+    fn into_missle(
+        self,
+        velocity: &LinearVelocity,
+        origin_transform: &Transform,
+        materials: &mut ResMut<Assets<ColorMaterial>>,
+        meshes: &mut ResMut<Assets<Mesh>>,
+        assets: &Res<MyAssets>,
+    ) -> Self::MissleBundle;
+}
 
-    let shape = Capsule2d::new(0.5, 2.);
-    let collider = Collider::capsule(0.5, 0.2);
-    let color = Color::srgb(7.5, 1.0, 7.5);
-    let material = materials.add(color);
-    let model2d = MaterialMesh2dBundle {
-        mesh: meshes.add(shape).into(),
-        transform,
-        material,
-        ..default()
-    };
+fn handle_plasma_gun_fire(
+    mut reader: EventReader<GunFire>,
+    mut cmds: Commands,
+    mut q: Query<(&mut PlasmaGun, &LinearVelocity, &Transform)>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+    mut meshes: ResMut<Assets<Mesh>>,
+    assets: Res<MyAssets>,
+) {
+    reader.read().for_each(|event| {
+        let Ok(res) = q.get_mut(event.entity) else {
+            return;
+        };
+        let (mut gun, ship_velocity, ship_transform) = res;
 
-    let settings = PlaybackSettings {
-        mode: bevy::audio::PlaybackMode::Despawn,
-        speed: 1.5,
-        volume: Volume::new(0.3),
-        ..Default::default()
-    };
-    let pew_sound = AudioBundle {
-        source: assets.laser_sound.clone(),
-        settings,
-    };
-    // audio
+        let Some(plasma) = gun.fire() else {
+            return;
+        };
 
-    cmds.spawn(pew_sound);
-    let missle = (
-        // moving_obj,
-        plasma,
-        ColliderDensity(Plasma::DENSITY),
-        RigidBody::Dynamic,
-        collider,
-        velocity,
-        model2d,
-        // HomeMadeCollider::new(0.1),
-        Health {
-            life: 1,
-            death_cry: DeathCry::Pop,
-        },
-        CollisionDamage(Plasma::DAMAGE),
-    );
-    cmds.spawn(missle);
+        let mut transform = ship_transform.with_scale(Vec3::ONE);
+        let velocity: LinearVelocity =
+            (-transform.up().truncate() * Plasma::SPEED + **ship_velocity).into();
+        transform.translation -= FORWARD_OFFSET * *ship_transform.up();
+
+        let shape = Capsule2d::new(0.5, 2.);
+        let collider = Collider::capsule(0.5, 0.2);
+        let color = Color::srgb(7.5, 1.0, 7.5);
+        let material = materials.add(color);
+        let model2d = MaterialMesh2dBundle {
+            mesh: meshes.add(shape).into(),
+            transform,
+            material,
+            ..default()
+        };
+
+        let settings = PlaybackSettings {
+            mode: bevy::audio::PlaybackMode::Despawn,
+            speed: 1.5,
+            volume: Volume::new(0.3),
+            ..Default::default()
+        };
+        let pew_sound = AudioBundle {
+            source: assets.laser_sound.clone(),
+            settings,
+        };
+        // audio
+
+        cmds.spawn(pew_sound);
+        let missle = (
+            // moving_obj,
+            plasma,
+            ColliderDensity(Plasma::DENSITY),
+            RigidBody::Dynamic,
+            collider,
+            velocity,
+            model2d,
+            // HomeMadeCollider::new(0.1),
+            Health {
+                life: 1,
+                death_cry: DeathCry::Pop,
+            },
+            CollisionDamage(Plasma::DAMAGE),
+        );
+        cmds.spawn(missle);
+    });
 }
 fn fire_hook(
     mut hook_gun: Mut<NinjaGun>,
