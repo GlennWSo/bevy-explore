36a2e1b36d71b375019b6b0053b21d7080524c2a
GlennWSo <sondellglenn@gmail.com>
2024-07-30 08:53:33 +0200

    pulled out plasma into new file

--- src/guns/mod.rs
+++ src/guns/mod.rs
@@ -1,5 +1,8 @@
 mod ninja;
+mod plasma;
 pub use self::ninja::NinjaGun;
+use self::plasma::Plasma;
+pub use self::plasma::PlasmaGun;
 
 use std::marker::PhantomData;
 
@@ -38,39 +41,6 @@ impl Plugin for GunPlugin {
     }
 }
 
-#[derive(Component)]
-pub struct Plasma;
-
-impl Plasma {
-    const SPEED: f32 = 80.0;
-    const DAMAGE: i32 = 10;
-    const DENSITY: f32 = 5.0;
-}
-
-#[derive(Component)]
-pub struct PlasmaGun {
-    fire_interval: f32,
-    count_down: f32,
-}
-
-impl PlasmaGun {
-    fn pew(&self, cmds: &mut Commands, assets: &Res<MyAssets>) {
-        let settings = PlaybackSettings {
-            mode: bevy::audio::PlaybackMode::Despawn,
-            speed: 1.5,
-            volume: Volume::new(0.3),
-            ..Default::default()
-        };
-        let pew_sound = AudioBundle {
-            source: assets.laser_sound.clone(),
-            settings,
-        };
-        // audio
-
-        cmds.spawn(pew_sound);
-    }
-}
-
 #[derive(Bundle)]
 pub struct MissleBundle<M: Material2d> {
     pub model: MaterialMesh2dBundle<M>,
@@ -87,84 +57,6 @@ pub trait FireCtrl {
     /// dt time since frame
     fn fire(&mut self) -> Option<Self::Missle>;
     fn cooldown(&mut self, dt: f32);
-}
-
-impl Default for PlasmaGun {
-    fn default() -> Self {
-        Self::new(Self::DEFAULT_RATE)
-    }
-}
-
-impl PlasmaGun {
-    const DEFAULT_RATE: f32 = 0.1;
-    /// fire rate from seconds interval
-    pub fn new(cooldown: f32) -> Self {
-        assert!(cooldown >= 0.0);
-        Self {
-            fire_interval: cooldown,
-            count_down: 0.0,
-        }
-    }
-}
-
-impl FireCtrl for PlasmaGun {
-    type Missle = Plasma;
-    fn fire(&mut self) -> Option<Plasma> {
-        if self.count_down <= 0.0 {
-            self.count_down = self.fire_interval;
-            Some(Plasma)
-        } else {
-            None
-        }
-    }
-
-    fn cooldown(&mut self, dt: f32) {
-        if self.count_down <= 0.0 {
-            return;
-        }
-        self.count_down -= dt;
-    }
-}
-
-impl SpawnMissle for PlasmaGun {
-    fn spawn_missle(
-        &self,
-        cmds: &mut Commands,
-        ship_velocity: &LinearVelocity,
-        origin: Transform,
-        materials: &mut ResMut<Assets<ColorMaterial>>,
-        meshes: &mut ResMut<Assets<Mesh>>,
-        assets: &Res<MyAssets>,
-    ) {
-        let radius = 0.5;
-        let length = 2.;
-        let shape = Capsule2d::new(radius, length);
-        let color = Color::srgb(7.5, 1.0, 7.5);
-        let material = materials.add(color);
-        let model = MaterialMesh2dBundle {
-            mesh: meshes.add(shape).into(),
-            transform: origin,
-            material,
-            ..default()
-        };
-        let velocity: LinearVelocity =
-            (-origin.up().truncate() * Plasma::SPEED + **ship_velocity).into();
-
-        let missle = MissleBundle {
-            model,
-            collider: Collider::capsule(radius, length),
-            rigidbody: RigidBody::Dynamic,
-            density: ColliderDensity(Plasma::DENSITY),
-            health: Health {
-                life: 1,
-                ..default()
-            },
-            damage: CollisionDamage(Plasma::DAMAGE),
-            velocity,
-        };
-        cmds.spawn(missle);
-        self.pew(cmds, assets);
-    }
 }
 
 #[derive(Event)]
@@ -247,65 +139,7 @@ fn handle_gun_fire<G: Gun>(
         );
     });
 }
-fn fire_hook(
-    mut hook_gun: Mut<NinjaGun>,
-    ship_transform: &Transform,
-    ship_velocity: &LinearVelocity,
-    materials: &mut ResMut<Assets<ColorMaterial>>,
-    meshes: &mut ResMut<Assets<Mesh>>,
-    assets: &Res<MyAssets>,
-    cmds: &mut Commands,
-) {
-    let Some(plasma) = hook_gun.fire() else {
-        return;
-    };
 
-    let mut transform = ship_transform.with_scale(Vec3::ONE);
-    let velocity: LinearVelocity =
-        (-transform.up().truncate() * Plasma::SPEED + **ship_velocity).into();
-    transform.translation -= FORWARD_OFFSET * *ship_transform.up();
-
-    let shape = Capsule2d::new(0.5, 2.);
-    let collider = Collider::capsule(0.5, 0.2);
-    let color = Color::srgb(7.5, 1.0, 7.5);
-    let material = materials.add(color);
-    let model2d = MaterialMesh2dBundle {
-        mesh: meshes.add(shape).into(),
-        transform,
-        material,
-        ..default()
-    };
-
-    let settings = PlaybackSettings {
-        mode: bevy::audio::PlaybackMode::Despawn,
-        speed: 1.5,
-        volume: Volume::new(0.3),
-        ..Default::default()
-    };
-    let pew_sound = AudioBundle {
-        source: assets.laser_sound.clone(),
-        settings,
-    };
-    // audio
-
-    cmds.spawn(pew_sound);
-    let missle = (
-        // moving_obj,
-        plasma,
-        ColliderDensity(Plasma::DENSITY),
-        RigidBody::Dynamic,
-        collider,
-        velocity,
-        model2d,
-        // HomeMadeCollider::new(0.1),
-        Health {
-            life: 1,
-            death_cry: DeathCry::Pop,
-        },
-        CollisionDamage(Plasma::DAMAGE),
-    );
-    cmds.spawn(missle);
-}
 fn cooldown_guns<T: FireCtrl + Component>(mut q: Query<&mut T>, time: Res<Time>) {
     let dt = time.delta_seconds();
     for mut gun in q.iter_mut() {
---
+++ src/guns/plasma.rs
@@ -0,0 +1,116 @@
+use crate::{collide_dmg::CollisionDamage, health::Health};
+
+use super::{FireCtrl, MissleBundle, MyAssets, SpawnMissle};
+
+use avian2d::prelude::*;
+use bevy::{audio::Volume, prelude::*, sprite::MaterialMesh2dBundle};
+
+#[derive(Component)]
+pub struct Plasma;
+
+impl Plasma {
+    const SPEED: f32 = 80.0;
+    const DAMAGE: i32 = 10;
+    const DENSITY: f32 = 5.0;
+}
+
+#[derive(Component)]
+pub struct PlasmaGun {
+    fire_interval: f32,
+    count_down: f32,
+}
+
+impl PlasmaGun {
+    fn pew(&self, cmds: &mut Commands, assets: &Res<MyAssets>) {
+        let settings = PlaybackSettings {
+            mode: bevy::audio::PlaybackMode::Despawn,
+            speed: 1.5,
+            volume: Volume::new(0.3),
+            ..Default::default()
+        };
+        let pew_sound = AudioBundle {
+            source: assets.laser_sound.clone(),
+            settings,
+        };
+        // audio
+
+        cmds.spawn(pew_sound);
+    }
+}
+impl Default for PlasmaGun {
+    fn default() -> Self {
+        Self::new(Self::DEFAULT_RATE)
+    }
+}
+
+impl PlasmaGun {
+    const DEFAULT_RATE: f32 = 0.1;
+    /// fire rate from seconds interval
+    pub fn new(cooldown: f32) -> Self {
+        assert!(cooldown >= 0.0);
+        Self {
+            fire_interval: cooldown,
+            count_down: 0.0,
+        }
+    }
+}
+
+impl FireCtrl for PlasmaGun {
+    type Missle = Plasma;
+    fn fire(&mut self) -> Option<Plasma> {
+        if self.count_down <= 0.0 {
+            self.count_down = self.fire_interval;
+            Some(Plasma)
+        } else {
+            None
+        }
+    }
+
+    fn cooldown(&mut self, dt: f32) {
+        if self.count_down <= 0.0 {
+            return;
+        }
+        self.count_down -= dt;
+    }
+}
+
+impl SpawnMissle for PlasmaGun {
+    fn spawn_missle(
+        &self,
+        cmds: &mut Commands,
+        ship_velocity: &LinearVelocity,
+        origin: Transform,
+        materials: &mut ResMut<Assets<ColorMaterial>>,
+        meshes: &mut ResMut<Assets<Mesh>>,
+        assets: &Res<MyAssets>,
+    ) {
+        let radius = 0.5;
+        let length = 2.;
+        let shape = Capsule2d::new(radius, length);
+        let color = Color::srgb(7.5, 1.0, 7.5);
+        let material = materials.add(color);
+        let model = MaterialMesh2dBundle {
+            mesh: meshes.add(shape).into(),
+            transform: origin,
+            material,
+            ..default()
+        };
+        let velocity: LinearVelocity =
+            (-origin.up().truncate() * Plasma::SPEED + **ship_velocity).into();
+
+        let missle = MissleBundle {
+            model,
+            collider: Collider::capsule(radius, length),
+            rigidbody: RigidBody::Dynamic,
+            density: ColliderDensity(Plasma::DENSITY),
+            health: Health {
+                life: 1,
+                ..default()
+            },
+            damage: CollisionDamage(Plasma::DAMAGE),
+            velocity,
+        };
+        cmds.spawn(missle);
+        self.pew(cmds, assets);
+    }
+}
