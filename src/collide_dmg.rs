25aae4a913ff1697cdb3c50e7e75a55c4793763c
GlennWSo <sondellglenn@gmail.com>
2024-08-02 22:14:56 +0200

    generics for collide damage to enable special handling of certen types

--- src/collide_dmg.rs
+++ src/collide_dmg.rs
@@ -1,37 +1,37 @@
 use avian2d::prelude::*;
-use bevy::prelude::*;
+use bevy::{ecs::query::QueryFilter, prelude::*};
 
-use crate::health::Health;
+use crate::{guns::Plasma, health::Health};
 
 pub struct CollideDamagePlugin;
 
 impl Plugin for CollideDamagePlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Update, collision_damage);
+        app.add_systems(Update, contact_damage::<With<Plasma>, Without<Plasma>>);
+        app.add_systems(Update, contact_damage::<Without<Plasma>, ()>);
     }
 }
 
 #[derive(Component, Deref, DerefMut)]
 pub struct CollisionDamage(pub i32);
 
-fn collision_damage(
+fn contact_damage<HealthFilter: QueryFilter, DmgFilter: QueryFilter>(
     mut collision_event_reader: EventReader<CollisionStarted>,
-    mut health_q: Query<&mut Health>,
-    damage_q: Query<&CollisionDamage>,
+    mut health_q: Query<&mut Health, HealthFilter>,
+    damage_q: Query<&CollisionDamage, DmgFilter>,
 ) {
-    for CollisionStarted(ent1, ent2) in collision_event_reader.read() {
-        let ent1 = *ent1;
-        let ent2 = *ent2;
-        if let (Ok(dmg), Ok(mut health)) = (damage_q.get(ent1), health_q.get_mut(ent2)) {
-            **health -= **dmg
+    {
+        let mut collision_event_reader = collision_event_reader;
+        let mut health_q = health_q;
+        for CollisionStarted(ent1, ent2) in collision_event_reader.read() {
+            let ent1 = *ent1;
+            let ent2 = *ent2;
+            if let (Ok(dmg), Ok(mut health)) = (damage_q.get(ent1), health_q.get_mut(ent2)) {
+                **health -= **dmg
+            }
+            if let (Ok(dmg), Ok(mut health)) = (damage_q.get(ent2), health_q.get_mut(ent1)) {
+                **health -= **dmg
+            }
         }
-        if let (Ok(dmg), Ok(mut health)) = (damage_q.get(ent2), health_q.get_mut(ent1)) {
-            **health -= **dmg
-        }
-
-        // println!(
-        //     "Entities {:?} and {:?} are colliding",
-        //     contacts.entity1, contacts.entity2,
-        // );
-    }
+    };
 }
